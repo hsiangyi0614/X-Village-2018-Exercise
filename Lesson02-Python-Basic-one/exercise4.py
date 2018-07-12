14adddeb7d0e59689f2686b94b1e4924e566726b
hsiangyi0614 <shawnyi0614@gmail.com>
2018-07-12 15:08:42 +0800

    0712-3

--- Lesson02-Python-Basic-one/exercise4.py
+++ Lesson02-Python-Basic-one/exercise4.py
@@ -1,4 +1,5 @@
  #用 * 印出直角三角形
+#method1
 i = 1
 j = 1
 while i <= 9:
@@ -9,3 +10,12 @@ while i <= 9:
     print("")
     j = 1
     i += 1
+#method2
+i = 1
+j = 1
+x = 0
+for i in range(1,7):
+    x +=1
+    for j in range(1,7-x):
+        print( '*', sep='', end='\t')
+    print("")
